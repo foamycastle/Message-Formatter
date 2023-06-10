5f919580d3adcaccedad73401ec351eca5768091
FoamyCastle <unclepong@gmail.com>
2023-06-10 04:03:49 -0500

    new tests

--- tests/MessageFormatterTest.php
+++ tests/MessageFormatterTest.php
@@ -20,4 +20,60 @@ class MessageFormatterTest extends TestCase
         $formatter=new MessageFormatter($message,$symbols);
         self::assertIsString((string)$formatter);
     }
+    public function test__invoke()
+    {
+        $message='this is a {good} message';
+        $symbols=[
+            'good'=>function(){
+                return 'buttfucked';
+            }
+        ];
+        $formatter=new MessageFormatter();
+        self::assertIsString((string)$formatter($message,$symbols));
+    }
+    public function testGetMessage()
+    {
+        $message='this is a {good} message';
+        $symbols=[
+            'good'=>function(){
+                return 'buttfucked';
+            }
+        ];
+        $formatter=new MessageFormatter($message,$symbols);
+        self::assertIsString($formatter($message,$symbols)->getMessage());
+    }
+    public function testAddSymbol(){
+        $message='this is a {symbol1} message. this is a {symbol2},this is a {symbol3}';
+        $symbols=[
+            'good'=>function(){
+                return 'buttfucked';
+            }
+        ];
+        $formatter=new MessageFormatter($message);
+        $formatter
+            ->addSymbol(['symbol1'=>'gorp'])
+            ->addSymbol(['symbol2','fuckup'])
+            ->addSymbol('symbol3','dildo');
+        self::assertGreaterThan(0,stripos($formatter,'gorp'));
+        self::assertGreaterThan(0,stripos($formatter,'fuckup'));
+        self::assertGreaterThan(0,stripos($formatter,'dildo'));
+        self::assertFalse(stripos($formatter,'buttfucked'));
+    }
+    public function testAddTemplate()
+    {
+        $message='this is a {symbol1} message. this is a [dope:,this is a [cheese:';
+        $symbols=[
+            'symbol1'=>function(){
+                return 'buttfucked';
+            }
+        ];
+        $formatter=new MessageFormatter($message,$symbols);
+        $formatter
+            ->addTemplate("[",":")
+            ->addSymbol('dope','fucking cool')
+            ->addSymbol('cheese','corn');
+        self::assertGreaterThan(0,stripos($formatter,'fucking cool'));
+        self::assertGreaterThan(0,stripos($formatter,'corn'));
+        self::assertGreaterThan(0,stripos($formatter,'buttfucked'));
+    }
 }
