0658816ab4c048fdb16e36942b300159b4b48e63
FoamyCastle <unclepong@gmail.com>
2023-08-15 16:04:06 -0500

    Symbol class documentation

--- src/Symbol.php
+++ src/Symbol.php
@@ -6,7 +6,15 @@ final class Symbol
 {
     public const DEFAULT_INITIATOR="{";
     public const DEFAULT_TERMINATOR="}";
+    /**
+     * The symbol identifier
+     * @var string|mixed
+     */
     public readonly string $name;
+    /**
+     * An object that contain the symbols data and renders it to a string
+     * @var SymbolData
+     */
     public readonly SymbolData $data;
 
     public function __construct($name, $data)
@@ -14,6 +22,12 @@ final class Symbol
         $this->name = self::validateName($name) ? $name : "";
         $this->data = SymbolData::New($data);
     }
+
+    /**
+     * Validate the characters contained in the name
+     * @param string $name The name must begin with [a-z]
+     * @return bool TRUE if validation passes, FALSE if not
+     */
     public static function validateName($name): bool
     {
         if (!is_string($name)) {
@@ -24,10 +38,23 @@ final class Symbol
         }
         return true;
     }
+
+    /**
+     * Forces the child classes to render the data to a string representation
+     * @return string
+     */
     public function __toString(): string
     {
         return (string)$this->data;
     }
+
+    /**
+     * Locates all symbols within a string.
+     * @param string $message
+     * @param string $initiator
+     * @param string $terminator
+     * @return SymbolTable|false
+     */
     public static function find(string &$message,string $initiator=self::DEFAULT_INITIATOR,string $terminator=self::DEFAULT_TERMINATOR):SymbolTable|false
     {
         $_initiator=preg_quote($initiator);
